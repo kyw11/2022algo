3b69971fbc7b483f3d37e797e8b1a4c7781a0ff5
강영원 <66764608+kyw11@users.noreply.github.com>
2022-11-01 22:54:28 +0900

    Add files via upload

---
+++ Problem1.java
@@ -0,0 +1,18 @@
+package day_1101;
+
+public class Problem1 {
+	public int solution(int[] pobi, int[] crong) {
+		boolean page = validetion(pobi, crong);
+		if(!page) {
+			
+			return -1;
+		}
+		return 0;
+	}
+
+	private boolean validetion(int[] pobi, int[] crong) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
