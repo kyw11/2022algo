9065f7ae8bd023f53ccfb9f8366b214cda857e4e
강영원 <66764608+kyw11@users.noreply.github.com>
2022-11-12 00:32:11 +0900

    Add files via upload

---
+++ BOJ1001A빼기B.java
@@ -0,0 +1,14 @@
+package day_1112;
+
+import java.util.Scanner;
+
+public class BOJ1001A빼기B {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println(A-B);
+	}
+
+}
