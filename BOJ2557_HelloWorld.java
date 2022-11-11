9ecd880ea97a2060ee863d5ef10810974713ea7d
강영원 <66764608+kyw11@users.noreply.github.com>
2022-11-12 00:21:16 +0900

    Add files via upload

---
+++ BOJ2557_HelloWorld.java
@@ -0,0 +1,12 @@
+package day_1112;
+
+public class BOJ2557_HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
+
