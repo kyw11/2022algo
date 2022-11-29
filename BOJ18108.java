715ed369d84863fc97b698a6635ba7231c632c93
강영원 <66764608+kyw11@users.noreply.github.com>
2022-11-29 16:57:01 +0900

    Add files via upload

---
+++ BOJ18108.java
@@ -0,0 +1,14 @@
+package day_1129;
+
+import java.util.Scanner;
+
+public class BOJ18108 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int by = sc.nextInt();
+		
+		System.out.println(by-543);
+		
+	}
+
+}
